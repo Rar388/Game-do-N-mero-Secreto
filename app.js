fe1b24ba26eee418a60f83f8ba93dc24f5d4fb4d
Raul Douglas <rauldouglasoliveira@gmail.com>
2025-08-03 12:32:58 -0300

    Fazendo mensagens no código

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Fazendo testes no código;
 let listaDeNumerosSorteados = [];
 let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
@@ -18,7 +19,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 }
 
 exibirMensagemInicial();
